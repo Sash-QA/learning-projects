280353da6a4204474c9c440e8811d298d4cbcd2a
Alexander Rakitin <cyberpunk024@gmail.com>
2024-01-22 01:59:04 +0500

    Добавление юнит-тестов

--- Calculator/Calculator.Tests/UnitTest1.cs
+++ Calculator/Calculator.Tests/UnitTest1.cs
@@ -1,16 +1,75 @@
+using NUnit.Framework;
+
 namespace Calculator.Tests
 {
-    public class Tests
+    [TestFixture]
+    public class CalculatorTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void Sum5and3_8expected()
         {
+            // Arrange
+            double firstNum = 5;
+            double secondNum = 3;
+
+            // Act
+            double result = Calculator.Add(firstNum, secondNum);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(8));
         }
 
         [Test]
-        public void Test1()
+        public void Sub5and3_2expected()
         {
-            Assert.Pass();
+            // Arrange
+            double firstNum = 5;
+            double secondNum = 3;
+
+            // Act
+            double result = Calculator.Subtract(firstNum, secondNum);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Mult5and3_15expected()
+        {
+            // Arrange
+            double firstNum = 5;
+            double secondNum = 3;
+
+            // Act
+            double result = Calculator.Multiply(firstNum, secondNum);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(15));
+        }
+
+        [Test]
+        public void Div6and3_2expected()
+        {
+            // Arrange
+            double firstNum = 6;
+            double secondNum = 3;
+
+            // Act
+            double result = Calculator.Divide(firstNum, secondNum);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void DivBy0_ExpectedException()
+        {
+            // Arrange
+            double firstNum = 6;
+            double secondNum = 0;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => Calculator.Divide(firstNum, secondNum));
         }
     }
 }
